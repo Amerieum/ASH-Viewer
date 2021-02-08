ee6db3f369beb97e031c628b259fd3010c538f91
aelhabbash <aelhabbash@AELHABBASH.emea.cegedim.grp>
2021-02-08 15:49:43 +0100

    Test commit

--- ashv/src/main/java/Main.java
+++ ashv/src/main/java/Main.java
@@ -4,7 +4,7 @@ import gui.MainWindow;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class Main {
+public class Main  {
     public static void main(String... args) {
         System.getProperties().setProperty("oracle.jdbc.J2EE13Compliant", "true");
 
